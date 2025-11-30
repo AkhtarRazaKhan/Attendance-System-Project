77bfaa9d1877c435cc70dec06a964da3b2facfe1
AkhtarRazaKhan <156532681+AkhtarRazaKhan@users.noreply.github.com>
2025-11-30 16:19:39 +0530

    Update Login.jsx

--- app/src/Login.jsx
+++ app/src/Login.jsx
@@ -29,7 +29,7 @@ const Login = () => {
 
     try {
       const response = await axios.post(
-        'http://localhost:5000/api/auth/login',
+        'https://attendance-system-project-60.onrender.com//api/auth/login',
         { email, password }
       );
 
